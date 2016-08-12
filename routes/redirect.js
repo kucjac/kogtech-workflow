916acb70b67226051eefe5fab94230a5305c531c
Rob Calcroft <rob@calcroft.com>
2016-08-12 12:23:02 +0100

    Cleanup

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,10 +1,11 @@
 import request from 'request';
+import debug from 'debug';
 
 export default function (req, res) {
   const code = req.query.code;
 
   request(`https://slack.com/api/oauth.access?client_id=${process.env.SLACK_CLIENT_ID}&client_secret=${process.env.SLACK_CLIENT_SECRET}&redirect_uri=${process.env.SLACK_REDIRECT_URI}&code=${code}`, (err, response) => {
-    console.log(response);
+    debug('oauth2')(response)
     res.status(200).end();
   });
 }
--- server.js
+++ server.js
@@ -14,21 +14,20 @@ app.use(bodyParser.json({ type: 'application/json' }));
 app.get('/redirect', redirect);
 
 // JIRA webhook handler
-app.post('/jira-webhook', function(req, res) {
-  console.log(`Incoming webhook at ${new Date()}`);
+app.post('/jira-webhook', (req, res) => {
+  debug('incoming')(`Incoming webhook at ${new Date()}`);
 
   // Switch case checks the issue type and runs the correct controller
   switch (req.body.issue_event_type_name) {
     case 'issue_assigned':
-      console.log('- Issue assigned');
       issueAssigned(req.body);
       break;
     // More cases can be added here to deal with various issue types
     default:
-      console.log(`- Unsupported issue type '${req.body.issue_event_type_name}'`);
+      debug('error')(`Unsupported issue type '${req.body.issue_event_type_name}'`);
   }
 });
 
-app.listen(process.env.PORT, function() {
-  console.log('Server running\n');
+app.listen(process.env.PORT, () => {
+  debug('info')(`Server running on port ${process.env.PORT}\n`);
 });
